e0b50a0b9a25793fdd1f1cec03356fe185cf5e03
Thiaggo Ferreira <thiaggo98@gmail.com>
2022-04-18 19:36:37 -0300

    Emergency Button, primeira versão

---
+++ HealthManager/src/Components/EmergencyButton/EmergencyButton.js
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import EmergencyIcon from "../../assets/icons/emergency.svg";
+import styles from "./styles";
+
+export default function EmergencyButton({ onPress }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.4}
+        onPress={onPress}
+      >
+        <EmergencyIcon width={42} height={42} style={styles.icon} />
+      </TouchableOpacity>
+      <Text style={styles.label}>Emergência</Text>
+    </View>
+  );
+}
---
+++ HealthManager/src/Components/EmergencyButton/styles.js
@@ -0,0 +1,31 @@
+﻿import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
+
+  button: {
+    width: 58,
+    height: 58,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFA708",
+    borderRadius: 50,
+    elevation: 4,
+  },
+
+  label: {
+    fontSize: 10,
+    fontFamily: "Roboto_400Regular",
+    color: "#E72727",
+
+    marginTop: 5,
+  },
+
+  icon: {
+    marginTop: 3,
+  },
+});
+
+export default styles;
