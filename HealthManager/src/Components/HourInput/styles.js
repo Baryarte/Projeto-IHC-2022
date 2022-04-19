6b652dbf789e1b0de2b31a57bd5f79d1adc3b830
Thiaggo Ferreira <thiaggo98@gmail.com>
2022-04-19 15:06:12 -0300

    HourInput criado

---
+++ HealthManager/src/Components/HourInput/styles.js
@@ -0,0 +1,48 @@
+﻿import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  label: {
+    fontFamily: "Roboto_100Thin",
+    fontSize: 24,
+    color: "#212f47",
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+
+  pickerRow: {
+    flexDirection: "row",
+  },
+
+  pickerContainer: {
+    // flex: 1,
+    justifyContent: "center",
+
+    borderWidth: 3,
+    borderRadius: 50,
+    borderColor: "#7EA1C4",
+    backgroundColor: "transparent",
+  },
+
+  colon: {
+    fontFamily: "Roboto_400Regular",
+    fontSize: 28,
+    top: 14,
+    marginHorizontal: 5,
+  },
+
+  picker: {
+    width: 93,
+    height: 65,
+  },
+
+  pickerItem: {
+    fontSize: 17,
+    fontFamily: "Roboto_400Regular",
+  },
+
+  pickerArrow: {
+    color: "#F1F5F9",
+  },
+});
+
+export default styles;
